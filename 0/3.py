032317ef379bffb317c91fdf01f85c7193b6fe6a
Allen <allenalvin333@gmail.com>
2021-07-05 06:36:43 +0530

    programs

--- 0/3.py
+++ 0/3.py
@@ -1,2 +1,8 @@
 # https://www.codechef.com/CCSTART2/problems/DIFACTRS
 
+s,x,y = "",0,int(input())
+for z in range(1,y+1):
+    if(y%z==0): 
+        s+=str(z)+" "
+        x+=1
+print(x,"\n",s,sep="")
