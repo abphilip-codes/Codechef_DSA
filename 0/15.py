457e0070e10c058c61099ccee9571b2103b26c47
Allen <allenalvin333@gmail.com>
2021-07-07 09:16:17 +0530

    programs

--- 0/15.py
+++ 0/15.py
@@ -1,1 +1,16 @@
 # https://www.codechef.com/CCSTART2/problems/SQALPAT
+
+n = int(input())
+j=1
+for i in range(1,n+1):
+    str1=str2=""
+    if(i%2!=0): 
+        while(j<=i*5):  
+            str1 = str1+str(j)+" "
+            j+=1 
+        print(str1.strip())
+    else:
+        while(j<=i*5):
+            str2 = str(j)+" "+str2
+            j+=1
+        print(str2.strip())
