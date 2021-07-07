b380744848fdc672fe72659768b72a7f526b5599
Allen <allenalvin333@gmail.com>
2021-07-07 13:26:08 +0530

    programs

---
+++ 0/11.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner in = new Scanner(System.in);
+        long n = in.nextInt();
+        System.out.println(n*(n+1)/2);
+    }
+}
--- 0/11.py
+++
@@ -1,4 +0,0 @@
-# https://www.codechef.com/CCSTART2/problems/ADDNATRL
-
-n = int(input())
-print(int((n*(n+1))/2))
