c5159bbfdf9dd197baa07f970440b4f1c3405e89
Allen <allenalvin333@gmail.com>
2021-07-07 08:38:00 +0530

    programs

--- 0/14.py
+++ 0/14.py
@@ -1,1 +1,5 @@
 # https://www.codechef.com/CCSTART2/problems/EXTRICHK
+
+l = sorted(list(map(int,input().split(" "))))
+if(l[2]<=sum(l[:2]) and all(x>0 for x in l)): print(len(set(l)))
+else: print("-1")
