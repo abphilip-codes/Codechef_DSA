ec43284af5f041da5bbdfb051a7eb7bdbd190761
Allen <allenalvin333@gmail.com>
2021-07-07 09:01:17 +0530

    programs

--- 0/9.py
+++ 0/9.py
@@ -1,4 +1,4 @@
 # https://www.codechef.com/CCSTART2/problems/TRIVALCH
 
 l = sorted(list(map(int,input().split(" "))))
-print((l[2]<=sum(l[:2])) and "YES" or "NO")
+print((l[2]<sum(l[:2])) and "YES" or "NO")
