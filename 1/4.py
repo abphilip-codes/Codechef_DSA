24adb209740bae05ceef7ce2006f6d407dc61012
Allen <allenalvin333@gmail.com>
2021-07-10 07:45:40 +0530

    programs

--- 1/4.py
+++ 1/4.py
@@ -1,2 +1,9 @@
 # https://www.codechef.com/LRNDSA01/problems/ZCO14003
 
+a = []
+m = 0
+for T in range(int(input())): a.append(int(input()))
+a.sort(reverse=True)
+for T in range(len(a)):
+    if(m<=(a[T]*(T+1))): m=a[T]*(T+1)
+print(m)
