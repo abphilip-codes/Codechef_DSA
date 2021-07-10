5cc0767d28690e27c2430bbdb8ded0d0cc6c5dab
Allen <allenalvin333@gmail.com>
2021-07-10 12:27:12 +0530

    programs

--- 1/6.py
+++ 1/6.py
@@ -1,2 +1,9 @@
 # https://www.codechef.com/LRNDSA01/problems/FCTRL
 
+for T in range(int(input())):
+    ans = 0
+    a = int(input())
+    while(a>=5):
+        a=a//5
+        ans=ans+a
+    print(ans)
