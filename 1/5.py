385aa65d1b26daac531ae97dca58cd88104b291a
Allen <allenalvin333@gmail.com>
2021-07-10 09:27:13 +0530

    programs

--- 1/5.py
+++ 1/5.py
@@ -4,6 +4,7 @@ for T in range(int(input())):
     ans = 1
     N = int(input())
     A = list(map(int, input().split()))
-    for i in range(1,len(A)):
+    for i in range(1,N):
         if(A[i]<=A[i-1]): ans+=1 
+        else: A[i]=A[i-1]
     print(ans)
