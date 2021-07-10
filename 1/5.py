98cf038b414df6d6eca1d01ed644800779d3874c
Allen <allenalvin333@gmail.com>
2021-07-10 07:51:28 +0530

    programs

--- 1/5.py
+++ 1/5.py
@@ -1,2 +1,9 @@
 # https://www.codechef.com/LRNDSA01/problems/CARVANS
 
+for T in range(int(input())):
+    ans = 1
+    N = int(input())
+    A = list(map(int, input().split()))
+    for i in range(1,len(A)):
+        if(A[i]<=A[i-1]): ans+=1 
+    print(ans)
