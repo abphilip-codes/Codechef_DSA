8bfab0d56f6f4b0311708fc7d11a06a7d38d771a
Allen <allenalvin333@gmail.com>
2021-07-12 09:00:28 +0530

    programs

--- 1/7.py
+++ 1/7.py
@@ -1,2 +1,11 @@
 # https://www.codechef.com/LRNDSA01/problems/CONFLIP
 
+for T in range(int(input())):
+    for G in range(int(input())):
+        I,N,Q = map(int,input().split())
+        if(N%2==0): head = N/2
+        else:
+            if(I==1): head = N//2
+            if(I==2): head = (N//2)+1
+        if(Q==1): print(int(head))
+        else: print(int(N-head))
