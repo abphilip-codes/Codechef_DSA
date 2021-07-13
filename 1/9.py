0f3d730e91efea6c49c4c100c49838be60baee83
Allen <allenalvin333@gmail.com>
2021-07-13 09:10:23 +0530

    programs

--- 1/9.py
+++ 1/9.py
@@ -1,1 +1,10 @@
 # https://www.codechef.com/LRNDSA01/problems/MULTHREE
+
+for T in range(int(input())):
+    k,d0,d1 = map(int,input().split())
+    A = (2*(d0+d1))%10 + (4*(d0+d1))%10 + (8*(d0+d1))%10 + (6*(d0+d1))%10
+    soln = d0+d1+((d0+d1)%10)+(A*((k-3)//4))
+    for i in range(1,((k-3)%4)+1):
+        soln = soln + ((2**i)*(d0+d1))%10
+    if(soln%3==0): print("YES")
+    else: print("NO")
